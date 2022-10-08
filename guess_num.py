5f702e7fe70f3586ae09d3c9281053adbbc73daa
alex810611 <alex810611@gmail.com>
2022-10-08 13:42:30 +0800

    base2

--- guess_num.py
+++ guess_num.py
@@ -4,16 +4,18 @@
 #猜錯要告訴他比答案大獲小
 import random
 r = random.randint(1, 100) # random int 
+count = 0
 while True :
+	count= count + 1 #count += 1
 	num = input ('請猜數字')
 	num = int(num)
 	if num == r :
 		print ('終於猜對了')
+		print('猜第幾',count ,'次')
 		break
 	elif num < r :
 		print('比答案小')
-
 	elif num > r :
 		print('比答案大')
-		
+	print('猜第幾',count ,'次')
 
